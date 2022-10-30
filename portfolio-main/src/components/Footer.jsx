785df397d285d9232ba5b8cd020ec03f6484181b
Chiranjit Behera <chiranjitbehera34@gmail.com>
2022-10-30 14:30:24 +0530

    init

--- portfolio-main/src/components/Footer.jsx
+++ portfolio-main/src/components/Footer.jsx
@@ -38,7 +38,6 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        {/* <div className="home__section__info" style={{}}>Designed & Built by<span>Chiranjit Behera</span> © 2022</div> */}
       </div>
     </section>
   );
